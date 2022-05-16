802a36155b2d0d8f8eae525d0c9632718a463dd3
Haiwei1027 <Haiwei.He@outlook.com>
2022-05-16 18:13:02 +0100

    stay on top and click through

--- DesktopBirdPet/Assets/TransparentWindow.cs
+++ DesktopBirdPet/Assets/TransparentWindow.cs
@@ -18,10 +18,27 @@ public class TransparentWindow : MonoBehaviour
     [DllImport("Dwmapi.dll")]
     private static extern uint DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS margins);
 
+    [DllImport("user32.dll")]
+    private static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
+
+    [DllImport("user32.dll")]
+    static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+    const int GWL_EXSTYLE = -20;
+
+    const uint WS_EX_LAYERED = 0x00080000;
+    const uint WS_EX_TRANSPARENT = 0x00000020;
+
+    static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+
     private void Start()
     {
         IntPtr hWnd = GetActiveWindow(); //Gets the ptr to the game window
         MARGINS margins = new MARGINS { cxLeftWidth = -1 }; //Negative margin for removing window frame
         DwmExtendFrameIntoClientArea(hWnd, ref margins); //Remove the window frame
+
+        SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED | WS_EX_TRANSPARENT);
+
+        SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, 0);
     }
 }
