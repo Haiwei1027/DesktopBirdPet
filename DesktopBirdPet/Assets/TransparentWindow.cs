cea6052c4bee34d5673e249105c6032158038a71
Haiwei1027 <Haiwei.He@outlook.com>
2022-05-16 17:06:41 +0100

    Transparent window

---
+++ DesktopBirdPet/Assets/TransparentWindow.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class TransparentWindow : MonoBehaviour
+{
+    private struct MARGINS
+    {
+        public int cxLeftWidth;
+        public int cxRightWidth;
+        public int cyTopHeight;
+        public int cyBottomHeight;
+    }
+
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetActiveWindow();
+
+    [DllImport("dwmap.api")]
+    private static extern uint DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS margins);
+
+    private void Start()
+    {
+        IntPtr hWnd = GetActiveWindow(); //Gets the ptr to the game window
+        MARGINS margins = new MARGINS { cxLeftWidth = -1 }; //Negative margin for removing window frame
+        DwmExtendFrameIntoClientArea(hWnd, ref margins); //Remove the window frame
+    }
+}
