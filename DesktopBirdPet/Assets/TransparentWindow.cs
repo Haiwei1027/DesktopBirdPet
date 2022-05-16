69a1c979437c3d12a4f1f292479cd21236f08328
Haiwei1027 <Haiwei.He@outlook.com>
2022-05-16 18:49:57 +0100

    refocuses when mouse is over an object

--- DesktopBirdPet/Assets/TransparentWindow.cs
+++ DesktopBirdPet/Assets/TransparentWindow.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class TransparentWindow : MonoBehaviour
 {
+    public LayerMask clickMask;
+
     private struct MARGINS
     {
         public int cxLeftWidth;
@@ -29,16 +31,54 @@ public class TransparentWindow : MonoBehaviour
     const uint WS_EX_LAYERED = 0x00080000;
     const uint WS_EX_TRANSPARENT = 0x00000020;
 
+    const uint LWA_COLORKEY = 0x00000001;
+
     static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+
+    private Camera camera;
+    bool mouseOver;
+    RaycastHit mouseOverInfo;
+
+    IntPtr hWnd;
 
     private void Start()
     {
-        IntPtr hWnd = GetActiveWindow(); //Gets the ptr to the game window
+        camera = Camera.main;
+#if !UNITY_EDITOR
+        hWnd = GetActiveWindow(); //Gets the ptr to the game window
         MARGINS margins = new MARGINS { cxLeftWidth = -1 }; //Negative margin for removing window frame
         DwmExtendFrameIntoClientArea(hWnd, ref margins); //Remove the window frame
 
-        SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED | WS_EX_TRANSPARENT);
+        SetClickthrough(true);
 
         SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, 0);
+
+        Application.runInBackground = true;
+#endif
+    }
+
+    private void Update()
+    {
+        mouseOver = CheckMouseover();
+        SetClickthrough(!mouseOver);
+    }
+
+    private bool CheckMouseover()
+    {
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction);
+        return Physics.Raycast(ray, out mouseOverInfo, 50, clickMask);
+    }
+
+    public void SetClickthrough(bool clickthrough)
+    {
+        if (clickthrough)
+        {
+            SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED | WS_EX_TRANSPARENT);
+        }
+        else
+        {
+            SetWindowLong(hWnd, GWL_EXSTYLE, WS_EX_LAYERED);
+        }
     }
 }
